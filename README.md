a41c2ae1032e7be18b13f3bad9128d7b6d0b0c5d
Steve Kinney <hello@stevekinney.net>
2015-05-02 20:54:45 -0600

    Reset the world and start over

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # Octavian
 
-Octavian is a super-simple utility function that takes a frequency and tells you what octave it's in.
-
-## Usage
-
-```js
-const octavian = require('octavian');
-
-octavian(440); // returns 4
-octavian(880); // returns 5
-```
-
-That's it.
+Octavian is a utility library for working with musical notes and their frequencies.
--- test/octavian-test.js
+++ test/octavian-test.js
@@ -1,27 +1,8 @@
-const test = require('./');
-const octavian = require('../');
+var chai = require('chai');
+var assert = chai.assert;
 
-test('It can figure out that A1 is in the first octave', function (t) {
-  t.equal(octavian(55), 1);
-  t.end();
-});
-
-test('It can figure out that A2 is in the second octave', function (t) {
-  t.equal(octavian(110), 2);
-  t.end();
-});
-
-test('It can figure out that A3 is in the third octave', function (t) {
-  t.equal(octavian(220), 3);
-  t.end();
-});
-
-test('It can figure out that A4 is in the fourth octave', function (t) {
-  t.equal(octavian(440), 4);
-  t.end();
-});
-
-test('It can figure out that A5 is in the fifth octave', function (t) {
-  t.equal(octavian(880), 5);
-  t.end();
+describe('the test suite', function () {
+  it('should works', function(){
+    assert(true);
+  });
 });
