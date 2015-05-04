f440f647727b5ad302c8b795e7f99feecbb3ba85
Steve Kinney <hello@stevekinney.net>
2015-05-03 22:19:48 -0600

    Adjust .gitignore

---
+++ build/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _interopRequireDefault = function (obj) { return obj && obj.__esModule ? obj : { 'default': obj }; };
+
+var _Note = require('./note');
+
+var _Note2 = _interopRequireDefault(_Note);
+
+exports['default'] = {
+  Note: _Note2['default']
+};
+module.exports = exports['default'];
